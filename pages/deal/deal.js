568aaf7a8e6e1b388bfa56a98955b9da0ae046ff
Jiechao Wang <jiewang@tripadvisor.com>
2018-07-14 19:02:09 -0400

    no message

--- pages/deal/deal.js
+++ pages/deal/deal.js
@@ -2,9 +2,12 @@ const app = getApp()
 
 Page({
   data: {
+    winWidth: 0,
+    winHeight: 0,
+    // tab切换
+    currentTab: 0,
     imageUrl: "http://img1.3lian.com/2015/w7/85/d/101.jpg",
-    dealCards: [
-      {
+    dealCards: [{
         index: 0,
         imageUrl: "http://img1.3lian.com/2015/w7/85/d/101.jpg",
         content: "海鲜大餐折扣活动，满 100 减 120 ，跳楼大减价",
@@ -23,6 +26,51 @@ Page({
         viewAndUseCount: "1000 人次查看　　100 人次使用"
       },
     ]
+  },
+
+  onLoad: function() {
+    var that = this;
+
+    /**
+     * 获取系统信息
+     */
+    wx.getSystemInfo({
+
+      success: function(res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+
+    });
+  },
+
+  /**
+   * 滑动切换tab
+   */
+  bindChange: function(e) {
+
+    var that = this;
+    that.setData({
+      currentTab: e.detail.current
+    });
+
+  },
+  /**
+   * 点击tab切换
+   */
+  swichNav: function(e) {
+
+    var that = this;
+
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
   },
 
   tapCard: function(e) {
